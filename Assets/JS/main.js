c60b6deab200aad8fbe5a86f07019defbb9e7b14
Script Kiddie <37413338+ScriptKiddie1337@users.noreply.github.com>
2018-06-23 09:53:56 -0700

    tutor help

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,7 +1,32 @@
 var searchTerm = "";
 var topics = ["Fortnite", "Half-Life", "Counter-Strike", "Halo", "Call of Duty", "Battlefield", "The Witcher", "Portal", "God of War", "Last of Us"];
 
-$("#gif-submit").on("click", function(event) {
+//parameters: topicsArray, classToAdd, buttonDisplay
+function buttonLoop(topicsArray, classToAdd, buttonDisplay) {
+    $(buttonDisplay).empty();
+    for (i = 0; i < topicsArray.length; i++) {
+        var btnSetup = $("<button>");
+        //"data-type" is coming from the Giphy API
+        btnSetup.attr("data-type", topicsArray[i]);
+        btnSetup.addClass(classToAdd);
+        btnSetup.text(topicsArray[i]);
+        $(buttonDisplay).append(btnSetup);
+    }
+}
+
+buttonLoop(topics, "add-class", "#btn-display");
+
+$("#gif-submit").on("click", function(){
+    console.log("#gif-submit");
+    //eq - look it up
+    var newGif = $('#gif-query').eq(0).val();
+    console.log(newGif);
+    topics.push(newGif);
+    buttonLoop(topics, "add-class", "#btn-display");
+    return false;
+})
+
+/* $("#gif-submit").on("click", function(event) {
     event.preventDefault();
 
     var gif = $("#gif-query").val();
@@ -11,4 +36,4 @@ var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=jDQFg
 $.ajax({
     url: queryURL,
     method: "GET"
-})
+}) */
